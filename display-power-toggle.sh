40dca328dbbe4746c03f55c385b0be6c49189a39
purpletortue <28994985+purpletortue@users.noreply.github.com>
2021-06-12 09:51:50 -0700

    add mqtt display state updates

--- display-power-toggle.sh
+++ display-power-toggle.sh
@@ -5,6 +5,7 @@
 vcgen_cmd='/usr/bin/vcgencmd'
 cut_cmd='/usr/bin/cut'
 sleep_cmd='/usr/bin/sleep'
+mqttpub_cmd='/usr/bin/mosquitto_pub'
 #grep_cmd='/usr/bin/grep'
 
 #vcgencmd outputs display_power=0 for off and =1 for on
@@ -17,6 +18,7 @@ if is_off
 then
   #turn on
   $vcgen_cmd display_power 1
+  $mqttpub_cmd -t frame/state -m on -q 0 -r
 
   # delay & restart monior process to prevent accidental multi touches from queueing
   $sleep_cmd 1
@@ -25,6 +27,7 @@ then
 else
   #turn off
   $vcgen_cmd display_power 0
+  $mqttpub_cmd -t frame/state -m off -q 0 -r
 
   # delay & restart monior process to prevent accidental multi touches from queueing
   $sleep_cmd 1
